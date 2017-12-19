049b807ff43e1e437e2772a225a42f092dd5a665
WKD622 <kuba.ziarko@gmail.com>
2017-12-19 19:26:50 +0100

    Delete JuniorDeveloper.java

--- src/jakubziarko/ideafactory/task1/JuniorDeveloper.java
+++
@@ -1,9 +0,0 @@
-package jakubziarko.ideafactory.task1;
-
-public class JuniorDeveloper extends AbstractEmployee{
-
-    JuniorDeveloper(String name, Role role) {
-        this.name=name;
-        this.role=role;
-    }
-}
