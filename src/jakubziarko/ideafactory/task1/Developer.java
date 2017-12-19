5d97138fc88027ff79c1f3465e3843fedb80b937
WKD622 <kuba.ziarko@gmail.com>
2017-12-19 19:26:59 +0100

    Delete Developer.java

--- src/jakubziarko/ideafactory/task1/Developer.java
+++
@@ -1,8 +0,0 @@
-package jakubziarko.ideafactory.task1;
-
-public class Developer extends AbstractEmployee {
-    Developer(String name, Role role) {
-        this.role=role;
-        this.name=name;
-    }
-}
